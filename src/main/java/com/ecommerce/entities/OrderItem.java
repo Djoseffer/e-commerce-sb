a7c38f0885615942d5f2e6ab78ea05dff06eda91
Djoseffer <tom_djosefer@hotmail.com>
2024-07-01 19:10:34 -0300

    feat: OrderItem, association with extra attributes

--- src/main/java/com/ecommerce/entities/OrderItem.java
+++ src/main/java/com/ecommerce/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package com.ecommerce.entities;
 
 import com.ecommerce.entities.Pk.OrderItemPk;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -32,6 +33,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
